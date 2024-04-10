86cbd9e416054365813a87164ceb8998fc120b47
Jonathan Orellana <Jonathan.Orellana2@nscorp.com>
2024-04-10 14:36:28 -0400

    New Main Function

--- DataConversions/dataconversion.go
+++ DataConversions/dataconversion.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-	var i int = 20
-	var f float64 = float64(i)
-	fmt.Println(i)
-	fmt.Println(f)
+// func main() {
+// 	var i int = 20
+// 	var f float64 = float64(i)
+// 	fmt.Println(i)
+// 	fmt.Println(f)
+// }
+
+func main(){
+	const value = 20
+	var i int = value
+	var f float64 = float64(value)
+
+	fmt.Println("Integer i =", i)
+	fmt.Println("Float f =",f)
 }
